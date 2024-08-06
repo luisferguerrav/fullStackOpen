1630825341c192546e5f353e7758bd503ad1a02d
lfgv <luisfernadogv@gmail.com>
2024-08-06 15:29:20 -0300

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+//*import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// const App = () =>
+//   {
+//     const now = "Agosto 6 a las 4 AM"
+//     const a = 10 
+//     const b = 20
+//     return React.createElement(
+//       "div", 
+//       null,
+//       React.createElement(
+//         "p", null, "hola world, it is ", now.toString()
+//       ),
+//       React.createElement(
+//         "p", null, a, " mas ", b, " es ", a+b
+//       )
+//     )
+//   }
+
+
+const Hello = (Props) => {
+    console.log(Props);
+    
+    
+  return(
+    <div>
+      <p> Hello {Props.name}, you are {Props.age} years old </p>
     </div>
-  );
+  )
 }
+
+  const App = () => {
+    const name  = "peter"
+    const age = 10 
+    
+    return(
+      <div>
+        <h1> Greetings</h1>
+        <Hello name = "maya" age ={ 26 + 10 }/>
+        <Hello name = {name} age ={age} />
+        
+      </div>
+    )
+  }
 
 export default App;
